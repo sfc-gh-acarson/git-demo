1fc35b339758ef63faa23d3cba791dfaad23260e
Andrew Carson <104647198+sfc-gh-acarson@users.noreply.github.com>
2024-04-09 08:17:06 -0400

    Delete sql/databases-schemas.sql

--- sql/databases-schemas.sql
+++
@@ -1,7 +0,0 @@
-use role sysadmin;
-
-create database if not exists dev_db;
-create database if not exists uat_db;
-create database if not exists prod_db;
-create database if not exists utility_db;
-create schema if not exists utility_db.admin_tools;
